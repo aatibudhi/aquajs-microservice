d422794796dd59e039f0e36bfea2a2adc1d00efd
uma96 <more.uma92@gmail.com>
2014-12-05 17:07:47 -0800

    updated tests with status code.

--- test/test.js
+++ test/test.js
@@ -34,20 +34,80 @@ var tests = [
   {
     testName: '200 test',
     statusName: 'OK'
+
+  },
+  {
+    testName: '201 test',
+    statusName: 'SUCCESS'
+  },
+  {
+    testName: '202 test',
+    statusName: 'ACCEPTED'
+  },
+  {
+    testName: '204 test',
+    statusName: 'noContent'
+  },
+  {
+    testName: '302 test',
+    statusName: 'FOUND'
+  },
+  {
+    testName: '304 test',
+    statusName: 'notModified'
+  },
+  {
+    testName: '400 test',
+    statusName: 'badRequest'
   },
   {
     testName: '401 test',
-    statusName: 'UNAUTHORIZED',
+    statusName: 'UNAUTHORIZED'
+  },
+  {
+    testName: '403 test',
+    statusName: 'FORBIDDEN'
+  },
+  {
+    testName: '404 test',
+    statusName: 'invalidRequest'
+  },
+  {
+    testName: '406 test',
+    statusName: 'invalidRequest'
+  },
+  {
+    testName: '410 test',
+    statusName: 'resourceGone'
+  },
+  {
+    testName: '429 test',
+    statusName: 'manyRequests'
+  },
+  {
+    testName: '500 test',
+    statusName: 'serverError'
+  },
+  {
+    testName: '502 test',
+    statusName: 'badGateway'
+  },
+  {
+    testName: '503 test',
+    statusName: 'serviceUnavailable'
+  },
+  {
+    testName: '504 test',
+    statusName: 'gatewayTimeout'
   }
 
 ];
 
 
-
 it('should verify all status codes', function (done) {
   var count = tests.length;
 
-  async.timesSeries(count, function(i, next) {
+  async.timesSeries(count, function (i, next) {
 
     var statusCode = statusCodes[tests[i].statusName].code;
 
@@ -57,7 +117,7 @@ it('should verify all status codes', function (done) {
       verify(res, statusCode, next);
     });
 
-  }, function() {
+  }, function () {
     done();
   });
 
