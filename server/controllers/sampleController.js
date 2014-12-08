fc197b5a384ffdb645df2f455222bb2e04e522f3
aatibudhi <Chakradhar1>
2014-12-08 14:29:52 -0800

    service scaffolding implementation

--- server/controllers/sampleController.js
+++ server/controllers/sampleController.js
@@ -1,6 +1,6 @@
-var thisAPImpl = null;
+var sampleControllerImpl = null;
 try {
-  thisAPImpl = require("../implementation/sample");
+  sampleServiceImpl = require("../impls/sampleServiceImpl");
 } catch (e) {
   // No implementation has been provided yet..
   // But that's OK because each end-point defined below will remind us...
@@ -8,26 +8,12 @@ try {
 
 var sampleController = {
   getApp: function(req, res) {
-    try {
-      return thisAPImpl.getApp(req, res);
-    } catch (e) {
-      var html = 'GET : Login with username and password <br/><br/>';
-      html += 'exports.<b>.getApp</b>.(req, res) {...}<br/><br/>';
-      html += 'should be exposed from within <b>server/implementation/sample.js</b><br/><br/>';
-      html += 'you can access <b>app.models[...]</b> to manipulate your data...';
-      res.send(html);
-    }
+    var response = sampleServiceImpl.getApp(req, res);
+    res.send(response);
   },
   postApp: function(req, res) {
-    try {
-      return thisAPImpl.postApp(req, res);
-    } catch (e) {
-      var html = 'POST : Register new application <br/><br/>';
-      html += 'exports.<b>.postApp</b>.(req, res) {...}<br/><br/>';
-      html += 'should be exposed from within <b>server/implementation/sample.js</b><br/><br/>';
-      html += 'you can access <b>app.models[...]</b> to manipulate your data...';
-      res.send(html);
-    }
+    var response = sampleServiceImpl.postApp(req, res);
+    res.send(response);
   }
 }
 module.exports = sampleController;
