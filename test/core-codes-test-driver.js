4bbd1ccc39d4df8b78bd80fd74d4f2aa96476382
umore <umore@equinix.com>
2014-12-10 16:55:26 -0800

    added extended error codes and test support

---
+++ test/core-codes-test-driver.js
@@ -0,0 +1,71 @@
+module.exports = [
+  {
+    testName: '200 test',
+    statusName: 'OK'
+  },
+  {
+    testName: '201 test',
+    statusName: 'SUCCESS'
+  },
+  {
+    testName: '202 test',
+    statusName: 'ACCEPTED'
+  },
+  {
+    testName: '204 test',
+    statusName: 'noContent'
+  },
+  {
+    testName: '302 test',
+    statusName: 'FOUND'
+  },
+  {
+    testName: '304 test',
+    statusName: 'notModified'
+  },
+  {
+    testName: '400 test',
+    statusName: 'badRequest'
+  },
+  {
+    testName: '401 test',
+    statusName: 'UNAUTHORIZED'
+  },
+  {
+    testName: '403 test',
+    statusName: 'FORBIDDEN'
+  },
+  {
+    testName: '404 test',
+    statusName: 'invalidRequest'
+  },
+  {
+    testName: '406 test',
+    statusName: 'invalidRequest'
+  },
+  {
+    testName: '410 test',
+    statusName: 'resourceGone'
+  },
+  {
+    testName: '429 test',
+    statusName: 'manyRequests'
+  },
+  {
+    testName: '500 test',
+    statusName: 'serverError'
+  },
+  {
+    testName: '502 test',
+    statusName: 'badGateway'
+  },
+  {
+    testName: '503 test',
+    statusName: 'serviceUnavailable'
+  },
+  {
+    testName: '504 test',
+    statusName: 'gatewayTimeout'
+  }
+
+];
