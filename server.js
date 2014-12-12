386850df9e5beec0e09f73698288942b2181f496
Tony Pujals <tony@subfuzion.com>
2014-12-12 13:55:28 -0800

    more server cleanup

--- server.js
+++ server.js
@@ -1,13 +1,15 @@
 GLOBAL.dirPaths = require(process.cwd() + '/server/config/dirPaths');
 
-var config = require(dirPaths.configDir + 'config');
+var path = require('path'),
+    bootstrap = require(path.join(dirPaths.configDir, 'system/bootstrap')),
+    config = require(path.join(dirPaths.configDir, 'config'));
 
-// Bootstrap app with Models, Dependencies, Routes configuration
-GLOBAL.app = require(dirPaths.configDir + 'system/bootstrap')(config);
+GLOBAL.app = bootstrap(config);
+GLOBAL.config = config;
 
 var logger = require('aquajs-logger').getLogger();
 
 var server = app.listen(config.port, function() {
-  logger.info("microservice listing on port:" + server.address().port);
+  logger.info("[microservice] listing on port:" + server.address().port);
 });
 
