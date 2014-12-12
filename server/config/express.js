66ddfeac7916d3e811354b385caf255166e1ade0
Tony Pujals <tony@subfuzion.com>
2014-12-12 12:20:05 -0800

    express cleanup

--- server/config/express.js
+++ server/config/express.js
@@ -1,4 +1,3 @@
-
 /**
  * Aqua JS Framework build on top of the express
  * Minimal Express initialization routing and env configuration files initialization
@@ -7,8 +6,6 @@
 
 'use strict'
 var express = require('express'),
-    session = require('express-session'),
-    flash = require('connect-flash'),
     helpers = require('view-helpers'),
     config = require('./config'),
     cookieParser = require('cookie-parser'),
@@ -28,77 +25,74 @@ var express = require('express'),
  */
 
 
-module.exports = function (app, db) {
+module.exports = function(app) {
 
+  // false for all environments except development
+  app.set('showStackError', false);
+
+  // Prettify HTML
+  app.locals.pretty = true;
+
+  // cache=memory or swig dies in NODE_ENV=production
+  if (process.env.NODE_ENV == 'production') {
+    app.locals.cache = 'memory';
+  }
+
+  // Should come before express.static (if used) to ensure content is compressed
+  app.use(compress({
+    filter: function(req, res) {
+      return (/json|text|javascript|css/).test(res.getHeader('Content-Type'));
+    },
+    // levels 0-9, where 0 = no compression (fastest) and 9 = best compression (slowest)
+    level: 9
+  }));
+
+  if (process.env.NODE_ENV === 'development') {
     app.set('showStackError', true);
+    app.use(morgan('dev'));
+  }
 
-    // Prettify HTML
-    app.locals.pretty = true;
+  app.enable('jsonp callback');
 
-    // cache=memory or swig dies in NODE_ENV=production
-    if (process.env.NODE_ENV == 'production') {
-        app.locals.cache = 'memory';
+  // Should come before session (if used)
+  app.use(cookieParser());
+
+  // Request body parsing middleware should be above method-override
+  // https://github.com/expressjs/body-parser#bodyparserurlencodedoptions
+  // extended: true means use qs for parsing
+  // https://www.npmjs.com/package/qs
+  app.use(bodyParser.urlencoded({extended: true}));
+  app.use(bodyParser.json());
+
+  app.use(require('method-override')())
+
+  // Dynamic helpers
+  app.use(helpers(config.app.name));
+
+  // Routes should be last
+  bootstrapRoutes(app);
+
+  //initializing the routes from the server/routes folder
+  function bootstrapRoutes(app) {
+    var routes_path = path.join(appPath, 'server', 'routes');
+    if (fs.existsSync(routes_path)) {
+      var walk = function(routes_path) {
+        fs.readdirSync(routes_path).forEach(function(file) {
+          var newPath = routes_path + '/' + file;
+          var stat = fs.statSync(newPath);
+          if (stat.isFile()) {
+            if (/(.*)\.(js$|coffee$)/.test(file)) {
+              require(newPath)(app);
+            }
+            // We skip the app/routes/middlewares directory as it is meant to be
+            // used and shared by routes as further middlewares and is not a
+            // route by itself
+          } else if (stat.isDirectory() && file !== 'middlewares') {
+            walk(newPath);
+          }
+        });
+      };
+      walk(routes_path);
     }
-
-    // Should be placed before express.static
-    // To ensure that all assets and data are compressed (utilize bandwidth)
-    app.use(compress({
-        filter: function (req, res) {
-            return (/json|text|javascript|css/).test(res.getHeader('Content-Type'));
-        },
-        // Levels are specified in a range of 0 to 9, where-as 0 is
-        // no compression and 9 is best compression, but slowest
-        level: 9
-    }));
-
-    // Only use logger for development environment
-    if (process.env.NODE_ENV === 'development') {
-        app.use(morgan('dev'));
-    }
-    // Enable jsonp
-    app.enable('jsonp callback');
-
-    // The cookieParser should be above session
-    app.use(cookieParser());
-
-    // Request body parsing middleware should be above methodOverride
-
-    app.use(bodyParser.urlencoded());
-
-    app.use(bodyParser.json());
-
-    app.use(require('method-override')())
-
-    // Dynamic helpers
-    app.use(helpers(config.app.name));
-
-    // Connect flash for flash messages
-    app.use(flash());
-
-    // Routes should be at the last
-    bootstrapRoutes(app);
-
-    //initializing the routes from the server/routes folder
-    function bootstrapRoutes(app) {
-        var routes_path = path.join(appPath, 'server','routes');
-        if(fs.existsSync(routes_path)) {
-            var walk = function (routes_path) {
-                fs.readdirSync(routes_path).forEach(function (file) {
-                    var newPath = routes_path + '/' + file;
-                    var stat = fs.statSync(newPath);
-                    if (stat.isFile()) {
-                        if (/(.*)\.(js$|coffee$)/.test(file)) {
-                            require(newPath)(app);
-                        }
-                        // We skip the app/routes/middlewares directory as it is meant to be
-                        // used and shared by routes as further middlewares and is not a
-                        // route by itself
-                    } else if (stat.isDirectory() && file !== 'middlewares') {
-                        walk(newPath);
-                    }
-                });
-            };
-            walk(routes_path);
-        }
-    }
+  }
 }
