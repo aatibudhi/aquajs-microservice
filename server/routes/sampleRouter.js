dd9affd8edc6d8312476e1eb3a9d8a544d924d9c
Tony Pujals <tony@subfuzion.com>
2014-12-12 07:36:52 -0800

    generated files shouldn't be versioned

--- server/routes/sampleRouter.js
+++
@@ -1,5 +0,0 @@
-var sampleRouter = function(app) {
-  var sampleController = require(path.join(dirPaths.controllersDir, 'sampleController'));
-  app.get('/application/:appid', sampleController.getApp);
-}
-module.exports = sampleRouter;
