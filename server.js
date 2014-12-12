4452fbc29e17a48a3bb7718d254b2ba8f6956756
Tony Pujals <tony@subfuzion.com>
2014-12-12 13:06:36 -0800

    removed redundant uncaughtException handler; it is already handled and logged

--- server.js
+++ server.js
@@ -7,13 +7,6 @@ GLOBAL.app = require(dirPaths.configDir + 'system/bootstrap')(config);
 
 var logger = require('aquajs-logger').getLogger();
 
-// Process uncaught Exception Before Exiting the program
-process.on('uncaughtException', function(err) {
-  console.error('An uncaughtException was found, the program will end.');
-  console.error(err);
-  process.exit(1);
-});
-
 var server = app.listen(config.port, function() {
   logger.info("microservice listing on port:" + server.address().port);
 });
