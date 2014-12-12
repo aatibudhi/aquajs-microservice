61ddc81c0c6d33a9ab4243daa75546765a042c97
Tony Pujals <tony@subfuzion.com>
2014-12-12 12:56:56 -0800

    more server cleanup

--- server.js
+++ server.js
@@ -1,50 +1,20 @@
-'use strict';
-/**
- * Created by verangasamy on 7/29/14.
- * Microservice follows the Scale Cube (specifically y-axis scaling) pattern and  functionally
- * decompose the application into a set of collaborating services. Each service implements a set of narrowly,
- * related functions. For example, an application might consist of services such as the order management service,
- * the customer management service etc.
- * Service communicate using either synchronous protocols such as HTTP/REST and run in different ports .
- * Services are developed and deployed independently of one another.
- */
+GLOBAL.dirPaths = require(process.cwd() + '/server/config/dirPaths');
 
-//Export Global Path Object
+var config = require(dirPaths.configDir + 'config');
 
-GLOBAL.dirPaths = require( process.cwd()+'/server/config/dirPaths');
+// Bootstrap app with Models, Dependencies, Routes configuration
+GLOBAL.app = require(dirPaths.configDir + 'system/bootstrap')(config);
 
-//require all the path variables
+var logger = require('aquajs-logger').getLogger();
 
-var path = require('path'),
-    express = require('express'),
-   // aqua = require('aquajs'),
-    http = require('http'),
-    bodyParser = require('body-parser'),
-    url = require("url"),
-    domain = require('domain'),
-
-// Initializing system variables
-    config = require(dirPaths.configDir + 'config');
-// Bootstrap Models, Dependencies, Routes and the app as an express app
-    global.app = require(dirPaths.configDir + 'system/bootstrap')(config);
-
-//Load NPM modules to memory using dependable's
-     //var aqua = aqua.app('aquajs'),
-//initialize the logger
-     var logger = require('aquajs-logger').getLogger();
-
-app.use(function (err, req, res, next) {
-    console.log(err);
-    next(err);
+// Process uncaught Exception Before Exiting the program
+process.on('uncaughtException', function(err) {
+  console.error('An uncaughtException was found, the program will end.');
+  console.error(err);
+  process.exit(1);
 });
 
-//Process uncaught Exception Before Exiting the program
-process.on('uncaughtException', function (err) {
-    console.error('An uncaughtException was found, the program will end.');
-    process.exit(1);
+var server = app.listen(config.port, function() {
+  logger.info("microservice listing on port:" + server.address().port);
 });
-
-app.listen(config.port);
-
-logger.info("Server Listing on the port:" + config.port);
 
