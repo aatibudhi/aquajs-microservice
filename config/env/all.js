04f338e6a6edc92780b9e8d4fa0faf161345387e
etuscano <etuscano@equinix.com>
2014-12-19 12:00:56 -0800

    auto-api generation merge

--- config/env/all.js
+++ config/env/all.js
@@ -18,7 +18,7 @@ module.exports = {
     apiServer: 'true',
     //Batch process server
     batchProcessServer: 'false',
-    enableWaterline:'false',
+    enableWaterline:'true',
     enablePersist:'false'
 
 };
