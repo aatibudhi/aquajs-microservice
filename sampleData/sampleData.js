dc8cb137f45acd53a3849059ef410b6fb5566402
etuscano <etuscano@equinix.com>
2014-12-23 16:57:26 -0800

    Sample data generation

---
+++ sampleData/sampleData.js
@@ -0,0 +1,28 @@
+
+//Sample data for orders
+exports.orderPost = {
+  "orderId": "100",
+  "orderName": "bbq",
+  "orderContents": "bbq chicken pizza",
+  "quantity": 1,
+  "orderStatus": "in progress"
+};
+
+exports.orderID = 100;
+
+exports.orderPut = {
+  "orderName": "veg",
+  "orderContents": "Pacific Veggie pizza",
+  "quantity": 2,
+  "orderStatus": "in progress"
+};
+
+
+//Sample data for pizza
+
+exports.pizzaPost = {
+  "pizzaId": "200",
+  "pizzaName": "veg",
+  "pizzaContents": "cheese,olives,peppers",
+  "Size": 6
+};
