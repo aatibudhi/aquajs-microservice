38aba457b0114c9c278135162bb9c59442d5e767
aatibudhi <Chakradhar1>
2015-01-19 16:44:43 -0800

    for sample data folder name change

---
+++ data/sampleData.js
@@ -0,0 +1,27 @@
+
+//Sample data for orders
+exports.orderPost = {
+  "id": 100,
+  "order_name": "bbq",
+  "order_contents": "bbq chicken pizza",
+  "quantity": 1,
+  "order_status": "in progress"
+};
+
+exports.orderID = 100;
+
+exports.orderPut = {
+  "order_name": "veg",
+  "order_contents": "Pacific Veggie pizza",
+  "quantity": 2,
+  "order_status": "in progress"
+};
+
+//Sample data for pizza
+
+exports.pizzaPost = {
+  "id": 50,
+  "pizza_name": "veggie",
+  "pizza_contents": "cheese,olives,peppers",
+  "Size": 12
+};
