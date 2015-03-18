3274d18920fa5e0f32130a53280cd1c302d9d762
Peter Svetlichny <psvetlichny@equinix.com>
2015-03-18 14:02:45 -0700

    Added implDir to dirPaths

--- config/dirPaths.js
+++ config/dirPaths.js
@@ -1,8 +1,9 @@
 var path = require('path');
 
 var serverDir = exports.serverDir = process.cwd();
+exports.implDir = path.join(serverDir, 'impls/');
 exports.configDir = path.join(serverDir, 'config/');
 exports.modelsDir = path.join(serverDir, 'models/');
 exports.routesDir = path.join(serverDir, 'routes/');
 exports.controllersDir = path.join(serverDir, 'controllers/');
-exports.nodeModulesDir = path.join(serverDir, 'node_modules/')
+exports.nodeModulesDir = path.join(serverDir, 'node_modules/');
